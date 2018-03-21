444981366154d9f25538bdc8a61da13ac1233899
Martin Broos <martin.broos@freshheads.com>
2018-03-21 15:40:09 +0100

    lib setup

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+
+const config = {
+    entry: __dirname + '/src/js/index.js',
+    devtool: 'source-map',
+    output: {
+        path: __dirname + '/lib',
+        filename: 'FHCookieGuard.js',
+        library: 'FHCookieGuard',
+        libraryTarget: 'umd',
+        umdNamedDefine: true
+    },
+    module: {
+        rules: [
+            {
+                test: /(\.jsx|\.js)$/,
+                exclude: /(node_modules)/,
+                use: [{
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['es2015', {
+                                modules: false
+                            }]
+                        ]
+                    }
+                }]
+            },
+            {
+                test: /(\.jsx|\.js)$/,
+                loader: 'eslint-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        modules: [path.resolve('./node_modules'), path.resolve('./src')],
+        extensions: ['.json', '.js']
+    }
+};
+
+module.exports = config;
