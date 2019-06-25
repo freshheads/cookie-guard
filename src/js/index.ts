3d1b2d57f1b95d78c7fcc2034074dfdcaa0b4609
remco-freshheads <remco.abalain@freshheads.com>
2019-06-25 12:05:35 +0200

    Revert target back to es5

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,4 +1,4 @@
-import { merge } from 'lodash-es';
+import { merge } from 'lodash';
 import CookieConsentStore from './CookieConsentStore';
 import CookieConsentNotification from './CookieConsentNotification';
 import AutoAcceptRequestWatcher from './AutoAcceptRequestWatcher';
