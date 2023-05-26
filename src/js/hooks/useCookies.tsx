fc2764e68e1e22d40a628e22de1fc9a557faa22c
Rens Alkemade <rens@Renss-MBP.freshheads.local>
2023-05-26 15:50:11 +0200

    moved state to context and refined banner

--- src/js/hooks/useCookies.tsx
+++ src/js/hooks/useCookies.tsx
@@ -2,7 +2,18 @@ import { useContext } from 'react';
 import { CookieGuardContext } from '../context/CookieGuardContext';
 
 export const useCookies = () => {
-    const { cookieSettings, setCookieSettings, clearCookieSettings } =
-        useContext(CookieGuardContext);
-    return { cookieSettings, setCookieSettings, clearCookieSettings };
+    const {
+        cookieSettings,
+        setCookieSettings,
+        clearCookieSettings,
+        cookieBannerIsOpen,
+        setCookieBannerIsOpen,
+    } = useContext(CookieGuardContext);
+    return {
+        cookieSettings,
+        setCookieSettings,
+        clearCookieSettings,
+        cookieBannerIsOpen,
+        setCookieBannerIsOpen,
+    };
 };
