8d035cfb53e43f0e4c24a357305f350a02ca5b52
Rens Alkemade <rens@Renss-MBP.freshheads.local>
2023-06-02 10:40:04 +0200

    added retract cookie logic and delete cookies

--- src/js/context/CookieGuardProvider.tsx
+++ src/js/context/CookieGuardProvider.tsx
@@ -2,24 +2,26 @@ import { FC, ReactNode, useEffect, useState } from 'react';
 import { setCookies as setBrowserCookies } from '../util/cookieFunctions';
 import { CookieGuardContext } from './CookieGuardContext';
 import Cookies from 'js-cookie';
-import { CookieCategorySettings } from '../types/cookies';
+import { CookieCategory, CookieCategorySettings } from '../types/cookies';
 
-export const cookieName = 'cookies_consent';
+export const cookieSettingsName = 'cookies_consent';
 
 export type CookieGuardsContextProviderProps = {
     children: ReactNode;
     onCookieSettingsChange?: (cookieSettings: CookieCategorySettings) => void;
     onCookieSettingsSet?: (cookieSettings: CookieCategorySettings) => void;
-    onCookieSettingsCleared?: () => void;
+    onCookieSettingsClear?: () => void;
+    reloadOnRetractCookies?: boolean;
 };
 
 export const CookieGuardProvider: FC<CookieGuardsContextProviderProps> = ({
     children,
     onCookieSettingsChange,
-    onCookieSettingsCleared,
     onCookieSettingsSet,
+    onCookieSettingsClear,
+    reloadOnRetractCookies = false,
 }) => {
-    const currentCookies = Cookies.get(cookieName);
+    const currentCookies = Cookies.get(cookieSettingsName);
     const initialState = currentCookies
         ? (JSON.parse(currentCookies) as CookieCategorySettings)
         : undefined;
@@ -30,6 +32,18 @@ export const CookieGuardProvider: FC<CookieGuardsContextProviderProps> = ({
 
     const [cookieSettings, setCookieSettings] =
         useState<CookieCategorySettings>(initialState);
+
+    const onCookieSettingRetract = () => {
+        /*
+            remove all cookies except the cookie that stores the cookie settings
+        */
+        Object.keys(Cookies.get()).forEach(function (cookie) {
+            if (cookie !== cookieSettingsName) {
+                Cookies.remove(cookie);
+            }
+        });
+        reloadOnRetractCookies && window.location.reload();
+    };
 
     useEffect(() => {
         onCookieSettingsChange && onCookieSettingsChange(cookieSettings);
@@ -42,6 +56,28 @@ export const CookieGuardProvider: FC<CookieGuardsContextProviderProps> = ({
     ) => {
         if (!newCookieSettings) return;
         if (Object.keys(newCookieSettings).length === 0) return;
+        let hasRetractedCookies = false;
+
+        /*
+            Since tags cannot be removed from the browser we need to refresh if a
+            cookie value is changed from true to false to remove all tags that were
+            set when the value was true.
+        */
+
+        if (cookieSettings) {
+            if (
+                (cookieSettings.analytics &&
+                    newCookieSettings.analytics === false) ||
+                (cookieSettings.marketing &&
+                    newCookieSettings.marketing === false) ||
+                (cookieSettings.functional &&
+                    newCookieSettings.functional === false) ||
+                (cookieSettings.required &&
+                    newCookieSettings.required === false)
+            ) {
+                hasRetractedCookies = true;
+            }
+        }
 
         const cookiesToSet = {
             ...cookieSettings,
@@ -50,7 +86,7 @@ export const CookieGuardProvider: FC<CookieGuardsContextProviderProps> = ({
         };
 
         setBrowserCookies(
-            cookieName,
+            cookieSettingsName,
             JSON.stringify(cookiesToSet),
             7,
             subdomains,
@@ -59,14 +95,20 @@ export const CookieGuardProvider: FC<CookieGuardsContextProviderProps> = ({
 
         setCookieSettings(cookiesToSet);
         onCookieSettingsSet && onCookieSettingsSet(cookiesToSet);
+        hasRetractedCookies && onCookieSettingRetract();
     };
 
     const clearCookieSettings = () => {
         if (typeof document === 'undefined') return;
         setCookieSettings(undefined);
-        Cookies.remove(cookieName);
-        // cookies in state is not updated immediately, so we need to pass undefined
-        onCookieSettingsCleared && onCookieSettingsCleared();
+        Cookies.remove(cookieSettingsName);
+        /*
+            Since tags cannot be removed from the browser we need to refresh if a
+            cookie value is changed from true to false to remove all tags that were
+            set when the value was true.
+        */
+        onCookieSettingsClear && onCookieSettingsClear();
+        onCookieSettingRetract();
     };
 
     return (
